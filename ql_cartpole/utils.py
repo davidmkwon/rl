7c5e61ffd95e49f14155c3809668ae78dc1683d2
davidmkwon <surval2001@gmail.com>
2020-06-09 22:24:28 -0400

    training plot function for utils

--- ql_cartpole/utils.py
+++ ql_cartpole/utils.py
@@ -1,44 +1,53 @@
 import matplotlib.pyplot as plt
 import json
 import random
+from collections import deque
 
 x = [1,2,3,8]
 y = [4,5,6,7]
 
-def plot():
+def plot_training(rewards, moving_avg_period=100):
     plt.figure(1)
-    plt.plot(x)
-    plt.plot(y)
-    plt.title("Training Period")
     plt.xlabel("Episode")
     plt.ylabel("Duration")
-    plt.savefig('res/training.png')
-    plt.show()
 
-def plot_training(values, moving_avg_period=100):
-    plt.figure(2)
-    plt.plot(x,y,'m')
-    plt.xlabel("Episode")
-    plt.ylabel("Duration")
-    plt.title("Testing Period")
-    plt.savefig("res/testing.png")
+    episodes = [i for i in range(1, len(rewards) + 1)]
+    num_1 = plt.plot(episodes, rewards, 'b', label="Episode Reward")
+
+    avg_queue = deque()
+    avg_rewards = deque()
+    for i in range(99):
+        avg_rewards.append(0)
+        avg_queue.append(rewards[i])
+    for i in range(100, len(rewards)):
+        avg_queue.append(rewards[i])
+        avg = sum(avg_queue) / len(avg_queue)
+        avg_rewards.append(avg)
+        avg_queue.popleft()
+    num_2 = plt.plot(episodes, avg_rewards, '--', label="100 Episode Moving Average")
+
+    plt.savefig("res/training.png", bbox_inches="tight")
+    plt.legend()
     plt.show()
 
 def plot_testing(rewards):
-    plt.figure(3)
-    episodes = [i for i in range(1, 101)]
+    plt.figure(2)
     plt.xlabel("Episode")
     plt.ylabel("Duration")
-    plt.title("Testing Period")
-    plt.plot(episodes, rewards, 'm')
-    avg_reward = sum(rewards) / 100
-    avg_rewards = [avg_reward for i in range(100)]
-    plt.plot(episodes, avg_rewards,'--')
-    plt.savefig("res/testing.png")
+
+    episodes = [i for i in range(1, len(rewards) + 1)]
+    num_1 = plt.plot(episodes, rewards, 'm', label="Episode Reward")
+
+    avg_reward = sum(rewards) / len(rewards)
+    avg_rewards = [avg_reward for i in range(len(rewards))]
+    num_2 = plt.plot(episodes, avg_rewards,'--', label="Average Reward")
+
+    plt.savefig("res/testing.png", bbox_inches='tight')
+    plt.legend()
     plt.show()
 
 def plot_epsilon(epsilons):
-    plt.figure(4)
+    plt.figure(3)
     episodes = [i for i in range(1, len(epsilons) + 1)]
     plt.xlabel("Episode")
     plt.ylabel("Epsilon")
@@ -70,4 +79,4 @@ def dummy(rewards):
     plt.show()
 
 rewards = [random.random() for i in range(100)]
-dummy(rewards)
+plot_testing(rewards)
